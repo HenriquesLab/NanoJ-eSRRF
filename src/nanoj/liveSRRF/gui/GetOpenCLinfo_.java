f80f125b1477f7cca6dd5a43b68a7a94e4155c62
Romain F. Laine <rfl30@cam.ac.uk>
2019-03-05 18:47:42 +0000

    Adding the Get OpenCL info tool

---
+++ src/nanoj/liveSRRF/gui/GetOpenCLinfo_.java
@@ -0,0 +1,41 @@
+package nanoj.liveSRRF.gui;
+
+import com.jogamp.opencl.CLContext;
+import com.jogamp.opencl.CLDevice;
+import com.jogamp.opencl.CLPlatform;
+import ij.IJ;
+import ij.plugin.PlugIn;
+
+public class GetOpenCLinfo_ implements PlugIn {
+
+    public void run(String arg) {
+
+        IJ.log("\\Clear");  // Clear the log window
+        IJ.log("-------------------------------------");
+        IJ.log("-------------------------------------");
+
+        CLPlatform[] allPlatforms = CLPlatform.listCLPlatforms();
+        IJ.log("Number of platforms: " + allPlatforms.length);
+        for (int p = 0; p < allPlatforms.length; p++) {
+            IJ.log("-----------------");
+            IJ.log("-----------------");
+            IJ.log("Platform #"+(p+1)+" ("+allPlatforms[p].getProfile()+")");
+            CLContext context = CLContext.create(allPlatforms[p]);
+
+            CLDevice[] allCLdevice = context.getDevices();
+
+            for (int i = 0; i < allCLdevice.length; i++) {
+                IJ.log("--------");
+                IJ.log("Device #" + i);
+                IJ.log("Max clock: " + allCLdevice[i].getMaxClockFrequency() + " MHz");
+                IJ.log("Number of compute units: " + allCLdevice[i].getMaxComputeUnits());
+                IJ.log("Device type: " + allCLdevice[i].getType());
+                IJ.log("Device name: " + allCLdevice[i].getName());
+            }
+
+            context.release();
+        }
+
+    }
+
+}
