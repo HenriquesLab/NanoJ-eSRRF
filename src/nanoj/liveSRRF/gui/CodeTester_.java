11aa99577303179049bb98b70d9a45ec4e7e7156
Romain F. Laine <rfl30@cam.ac.uk>
2020-01-15 18:09:45 +0000

    Added files from TimeConstantEstimation

---
+++ src/nanoj/liveSRRF/gui/CodeTester_.java
@@ -0,0 +1,32 @@
+package nanoj.liveSRRF.gui;
+
+import ij.IJ;
+import ij.gui.NonBlockingGenericDialog;
+import ij.plugin.PlugIn;
+
+
+public class CodeTester_ implements PlugIn {
+
+
+
+    public void run(String arg) {
+
+
+        NonBlockingGenericDialog gd = new NonBlockingGenericDialog("Fluorescence simulator");
+        gd.addNumericField("Number of frames: ", 10, 0);
+        gd.addNumericField("Exposure time (ms): ", 10.0, 1);
+        gd.showDialog();
+
+        int nFrames = (int) gd.getNextNumber();
+        double exposure = (double) gd.getNextNumber();
+        exposure /= 1000f;
+
+        double simDuration = (double) nFrames * exposure; // in seconds
+        IJ.log("Acquisition time: "+simDuration+"s ("+nFrames+" frames at "+(exposure*1000)+"ms exposure)");
+
+
+
+
+
+    }
+}
